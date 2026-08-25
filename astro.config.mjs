aa1c853b8d8d8a3410fcd3e858b3a89300261286
dor-chaouat <dorch@wix.com>
2026-08-25 16:01:49 +0300

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from "astro/config";
 import wix from "@wix/astro";
 import wixPages from "@wix/astro-pages";
 import react from "@astrojs/react";
-import wixHostingAdapter from "@wix/astro-wix-hosting-adapter";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: wixHostingAdapter(),
+  adapter: node({ mode: 'standalone' }),
   integrations: [wix(), wixPages(), react()],
   security: { checkOrigin: false }
 });
